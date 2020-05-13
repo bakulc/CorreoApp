121b16fb20bc90dc7b3bc1eb000138f4e3502194
Bakul Choudhary <choudharybakul809kv@gmail.com>
2020-05-13 18:59:19 +0530

    Finish Main Page Activity: Logout

--- app/src/main/java/com/example/correoapp/MainPageActivity.java
+++ app/src/main/java/com/example/correoapp/MainPageActivity.java
@@ -2,7 +2,10 @@ package com.example.correoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainPageActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        Button mLogout = findViewById(R.id.logout);
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
